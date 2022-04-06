48fe44b0b98de0a33437afae5d479b25bbeb87a8
Reza Kashkoul <rezakashkoul@gmail.com>
2022-04-06 17:55:33 +0430

    Added tests for NoteViewController, and also added a library to handle keyboard events, fixed some issues,added place holder for textView in an extension.

--- Bag-Pack/Controllers/NoteViewController.swift
+++ Bag-Pack/Controllers/NoteViewController.swift
@@ -14,7 +14,7 @@ class NoteViewController: UIViewController, UITextViewDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
         textView.delegate = self
-        //        textView.keyboardDismissMode = .onDrag
+//        textView.keyboardDismissMode = .onDrag
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -70,61 +70,3 @@ extension NoteViewController {
     }
 }
 
-extension UITextView: UITextViewDelegate {
-    
-    override open var bounds: CGRect {
-        didSet {
-            self.resizePlaceholder()
-        }
-    }
-    
-    public var placeholder: String? {
-        get {
-            var placeholderText: String?
-            
-            if let placeholderLabel = self.viewWithTag(100) as? UILabel {
-                placeholderText = placeholderLabel.text
-            }
-            
-            return placeholderText
-        }
-        set {
-            if let placeholderLabel = self.viewWithTag(100) as! UILabel? {
-                placeholderLabel.text = newValue
-                placeholderLabel.sizeToFit()
-            } else {
-                self.addPlaceholder(newValue!)
-            }
-        }
-    }
-    
-    public func textViewDidChange(_ textView: UITextView) {
-        if let placeholderLabel = self.viewWithTag(100) as? UILabel {
-            placeholderLabel.isHidden = !self.text.isEmpty
-        }
-    }
-    
-    private func resizePlaceholder() {
-        if let placeholderLabel = self.viewWithTag(100) as! UILabel? {
-            let labelX = self.textContainer.lineFragmentPadding
-            let labelY = self.textContainerInset.top - 2
-            let labelWidth = self.frame.width - (labelX * 2)
-            let labelHeight = placeholderLabel.frame.height
-            placeholderLabel.frame = CGRect(x: labelX, y: labelY, width: labelWidth, height: labelHeight)
-        }
-    }
-    
-    private func addPlaceholder(_ placeholderText: String) {
-        let placeholderLabel = UILabel()
-        placeholderLabel.text = placeholderText
-        placeholderLabel.sizeToFit()
-        placeholderLabel.font = self.font
-        placeholderLabel.textColor = UIColor.lightGray
-        placeholderLabel.tag = 100
-        placeholderLabel.isHidden = !self.text.isEmpty
-        self.addSubview(placeholderLabel)
-        self.resizePlaceholder()
-        self.delegate = self
-    }
-    
-}
---
+++ Bag-Pack/Extensions/Extensions.swift
@@ -0,0 +1,68 @@
+//
+//  Extensions.swift
+//  Bag-Pack
+//
+//  Created by Reza Kashkoul on 17/1/1401 AP.
+//
+
+import Foundation
+import UIKit
+
+extension UITextView: UITextViewDelegate {
+    
+    override open var bounds: CGRect {
+        didSet {
+            self.resizePlaceholder()
+        }
+    }
+    
+    public var placeholder: String? {
+        get {
+            var placeholderText: String?
+            
+            if let placeholderLabel = self.viewWithTag(100) as? UILabel {
+                placeholderText = placeholderLabel.text
+            }
+            
+            return placeholderText
+        }
+        set {
+            if let placeholderLabel = self.viewWithTag(100) as! UILabel? {
+                placeholderLabel.text = newValue
+                placeholderLabel.sizeToFit()
+            } else {
+                self.addPlaceholder(newValue!)
+            }
+        }
+    }
+    
+    public func textViewDidChange(_ textView: UITextView) {
+        if let placeholderLabel = self.viewWithTag(100) as? UILabel {
+            placeholderLabel.isHidden = !self.text.isEmpty
+        }
+    }
+    
+    private func resizePlaceholder() {
+        if let placeholderLabel = self.viewWithTag(100) as! UILabel? {
+            let labelX = self.textContainer.lineFragmentPadding
+            let labelY = self.textContainerInset.top - 2
+            let labelWidth = self.frame.width - (labelX * 2)
+            let labelHeight = placeholderLabel.frame.height
+            placeholderLabel.frame = CGRect(x: labelX, y: labelY, width: labelWidth, height: labelHeight)
+        }
+    }
+    
+    private func addPlaceholder(_ placeholderText: String) {
+        let placeholderLabel = UILabel()
+        placeholderLabel.text = placeholderText
+        placeholderLabel.sizeToFit()
+        placeholderLabel.font = self.font
+        placeholderLabel.textColor = UIColor.lightGray
+        placeholderLabel.tag = 100
+        placeholderLabel.isHidden = !self.text.isEmpty
+        self.addSubview(placeholderLabel)
+        self.resizePlaceholder()
+        self.delegate = self
+    }
+    
+}
