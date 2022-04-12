242147cbbce68cb12d958d0b643b38d2a4f196ba
Reza Kashkoul <rezakashkoul@gmail.com>
2022-04-12 18:43:09 +0430

    Some fixes...

--- Bag-Pack/Controllers/NewEntryViewController.swift
+++ Bag-Pack/Controllers/NewEntryViewController.swift
@@ -20,11 +20,40 @@ class NewEntryViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+       
+        setupUI()
 
-        confirmButton.layer.cornerRadius = 12
-        confirmButton.clipsToBounds = true
-        title = "Bag-Pack"
     }
 
-
+    func setupUI() {
+        confirmButton.layer.cornerRadius = 15
+        confirmButton.clipsToBounds = true
+        confirmButton.layer.borderWidth = 2.0
+        confirmButton.layer.cornerRadius = 15
+        confirmButton.layer.borderColor = appGlobalTintColor?.cgColor ?? UIColor.systemBlue.cgColor
+        titleTextField.borderStyle = .bezel
+        titleTextField.layer.masksToBounds = true
+        titleTextField.layer.borderColor = appGlobalTintColor?.cgColor ?? UIColor.systemBlue.cgColor
+        titleTextField.layer.borderWidth = 2.0
+        titleTextField.layer.cornerRadius = 15
+        titleTextField.clipsToBounds = true
+        placeTextField.borderStyle = .roundedRect
+        placeTextField.layer.masksToBounds = true
+        placeTextField.layer.borderColor = appGlobalTintColor?.cgColor ?? UIColor.systemBlue.cgColor
+        placeTextField.layer.borderWidth = 2.0
+        placeTextField.layer.cornerRadius = 15
+        placeTextField.clipsToBounds = true
+        budgetTextField.borderStyle = .roundedRect
+        budgetTextField.layer.masksToBounds = true
+        budgetTextField.layer.borderColor = appGlobalTintColor?.cgColor ?? UIColor.systemBlue.cgColor
+        budgetTextField.layer.borderWidth = 2.0
+        budgetTextField.layer.cornerRadius = 15
+        budgetTextField.clipsToBounds = true
+        daysTextField.borderStyle = .roundedRect
+        daysTextField.layer.masksToBounds = true
+        daysTextField.layer.borderColor = appGlobalTintColor?.cgColor ?? UIColor.systemBlue.cgColor
+        daysTextField.layer.borderWidth = 2.0
+        daysTextField.layer.cornerRadius = 15
+        daysTextField.clipsToBounds = true
+    }
 }
