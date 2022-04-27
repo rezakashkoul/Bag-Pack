3264cff2d9166f29e1c191ba7ddc6ef8d12ee086
Reza Kashkoul <rezakashkoul@gmail.com>
2022-04-27 13:44:46 +0430

    Added a deitail UI test for app.

--- Bag-PackUITests/Bag_PackUITestsLaunchTests.swift
+++ Bag-PackUITests/Bag_PackUITestsLaunchTests.swift
@@ -27,6 +27,18 @@ class Bag_PackUITestsLaunchTests: XCTestCase {
 //        add(attachment)
 //    }
     
+//    func test_shit(){
+//
+//        let app = XCUIApplication()
+//        app.navigationBars["BagPack"].buttons["Settings"].tap()
+//        app.tables/*@START_MENU_TOKEN@*/.staticTexts["Tint Color"]/*[[".cells.staticTexts[\"Tint Color\"]",".staticTexts[\"Tint Color\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
+//
+//        let elementsQuery = app.scrollViews.otherElements
+//        elementsQuery.otherElements["green 54"].tap()
+//        elementsQuery.buttons["close"].tap()
+//
+//    }
+    
     func test_addingANewTrip() {
         
         let app = XCUIApplication()
@@ -45,7 +57,7 @@ class Bag_PackUITestsLaunchTests: XCTestCase {
         addEntryButton.tap()
         
         let alertQuery = app.alerts["Add new item"].scrollViews.otherElements.collectionViews.textFields["Write an item to remember"]
-        alertQuery.typeText("ABCD")
+        alertQuery.typeText("Charger")
         let addButton = app.alerts["Add new item"].scrollViews.otherElements.buttons["Add"]
         addButton.tap()
         
@@ -54,7 +66,7 @@ class Bag_PackUITestsLaunchTests: XCTestCase {
         app.navigationBars["Costs"].buttons["Add"].tap()
         
         let titleAlertQuery = app.alerts["Add items"].scrollViews.otherElements.collectionViews.textFields["Title of purchase"]
-        titleAlertQuery.typeText("ABCDEFGHI")
+        titleAlertQuery.typeText("Tickets")
         
         let priceAlertQuery = app.alerts["Add items"].scrollViews.otherElements.collectionViews.textFields["Price"]
         priceAlertQuery.tap()
@@ -66,8 +78,16 @@ class Bag_PackUITestsLaunchTests: XCTestCase {
         tabBar.buttons["Note"].tap()
         app.textViews.containing(.staticText, identifier:"Type here...").element.tap()
 
-        let gKey = app/*@START_MENU_TOKEN@*/.keys["G"]/*[[".keyboards.keys[\"G\"]",".keys[\"G\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/
-        gKey.tap()
+        app.keys["F"].tap()
+        app.keys["a"].tap()
+        app.keys["n"].tap()
+        app.keys["t"].tap()
+        app.keys["a"].tap()
+        app.keys["s"].tap()
+        app.keys["t"].tap()
+        app.keys["i"].tap()
+        app.keys["c"].tap()
+        
         app.toolbars["Toolbar"].buttons["Done"].tap()
 
         deleteEntry(app)
@@ -80,13 +100,23 @@ class Bag_PackUITestsLaunchTests: XCTestCase {
         
         let settings = app.navigationBars.buttons.element(boundBy: 0)
         settings.tap()
+        
         let tablesQuery = app.tables
+        tablesQuery/*@START_MENU_TOKEN@*/.staticTexts["Reset Settings"]/*[[".cells.staticTexts[\"Reset Settings\"]",".staticTexts[\"Reset Settings\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
+        app.alerts["Reset application Settings"].scrollViews.otherElements.buttons["Reset Settings"].tap()
+        
+        tablesQuery.switches["ThemeSwitch"].tap()
+        
         tablesQuery/*@START_MENU_TOKEN@*/.buttons["Rials"]/*[[".cells",".segmentedControls.buttons[\"Rials\"]",".buttons[\"Rials\"]"],[[[-1,2],[-1,1],[-1,0,1]],[[-1,2],[-1,1]]],[0]]@END_MENU_TOKEN@*/.tap()
         tablesQuery/*@START_MENU_TOKEN@*/.buttons["€"]/*[[".cells",".segmentedControls.buttons[\"€\"]",".buttons[\"€\"]"],[[[-1,2],[-1,1],[-1,0,1]],[[-1,2],[-1,1]]],[0]]@END_MENU_TOKEN@*/.tap()
         tablesQuery/*@START_MENU_TOKEN@*/.buttons["$"]/*[[".cells",".segmentedControls.buttons[\"$\"]",".buttons[\"$\"]"],[[[-1,2],[-1,1],[-1,0,1]],[[-1,2],[-1,1]]],[0]]@END_MENU_TOKEN@*/.tap()
-        tablesQuery.switches["ThemeSwitch"].tap()
-        tablesQuery/*@START_MENU_TOKEN@*/.staticTexts["Reset Settings"]/*[[".cells.staticTexts[\"Reset Settings\"]",".staticTexts[\"Reset Settings\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
-        app.alerts["Reset application Settings"].scrollViews.otherElements.buttons["Reset Settings"].tap()
+        
+        tablesQuery/*@START_MENU_TOKEN@*/.staticTexts["Tint Color"]/*[[".cells.staticTexts[\"Tint Color\"]",".staticTexts[\"Tint Color\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
+        let elementsQuery = app.scrollViews.otherElements
+        elementsQuery.otherElements["green 54"].tap()
+        elementsQuery.buttons["close"].tap()
+        
+        
         tablesQuery/*@START_MENU_TOKEN@*/.staticTexts["Reset Application Data"]/*[[".cells.staticTexts[\"Reset Application Data\"]",".staticTexts[\"Reset Application Data\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
         app.alerts["Reset entire data"].scrollViews.otherElements.buttons["Reset data"].tap()
         settings.tap()
@@ -102,16 +132,28 @@ class Bag_PackUITestsLaunchTests: XCTestCase {
         
 //        let strings = ["A", "B", "C", "D","E", "F", "G", "H"]
 //        let aKey = app.keys[strings.randomElement()!]
-        let aKey = app.keys["A"]
-        aKey.tap()
+        app.keys["S"].tap()
+        app.keys["h"].tap()
+        app.keys["o"].tap()
+        app.keys["m"].tap()
+        app.keys["a"].tap()
+        app.keys["l"].tap()
+
         elementsQuery.textFields["Place Name"].tap()
-//        let bKey = app.keys[strings.randomElement()!]
-        let bKey = app.keys["B"]
-        bKey.tap()
+        app.keys["M"].tap()
+        app.keys["a"].tap()
+        app.keys["s"].tap()
+        app.keys["a"].tap()
+        app.keys["l"].tap()
+
         elementsQuery.textFields["Desired Budget"].tap()
         
-        let key = app/*@START_MENU_TOKEN@*/.keys["1"]/*[[".keyboards.keys[\"1\"]",".keys[\"1\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/
-        key.tap()
+        app.keys["1"].tap()
+        app.keys["0"].tap()
+        app.keys["0"].tap()
+        app.keys["0"].tap()
+        app.keys["0"].tap()
+
         elementsQuery.textFields["Day(s)"].tap()
         
         let key2 = app/*@START_MENU_TOKEN@*/.keys["2"]/*[[".keyboards.keys[\"2\"]",".keys[\"2\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/
@@ -124,7 +166,7 @@ class Bag_PackUITestsLaunchTests: XCTestCase {
         homeButton.tap()
         let tablesQuery = app.tables
         let staticText = tablesQuery/*@START_MENU_TOKEN@*/.staticTexts["2 Days"]/*[[".cells.staticTexts[\"2 Days\"]",".staticTexts[\"2 Days\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/
-        tablesQuery.cells.containing(.staticText, identifier:"A").element.swipeLeft()
+        tablesQuery.cells.containing(.staticText, identifier:"Shomal").element.swipeLeft()
         staticText.swipeLeft()
         tablesQuery/*@START_MENU_TOKEN@*/.buttons["Delete"]/*[[".cells.buttons[\"Delete\"]",".buttons[\"Delete\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
     }
